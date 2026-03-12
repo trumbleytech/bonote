c4ff0d1a8c3a2b149a8bc427c80155c6b40e251c
DPC <dyatrumbley@gmail.com>
2026-03-11 23:22:00 -0500

    adding db config

--- config.go
+++ config.go
@@ -3,7 +3,7 @@ package main
 import "os"
 
 var (
-	PORT string
+	PORT, DB_HOST, DB_PORT, DB_NAME, DB_USER, DB_PASS, DB_SSL string
 )
 
 // creating function to allow default val of undef env vars
@@ -18,4 +18,10 @@ func GetEnv(var_name, def_val string) string {
 // use init to pull in env vars
 func init() {
 	PORT = GetEnv("PORT", "3000")
+	DB_HOST = GetEnv("DB_HOST", "")
+	DB_PORT = GetEnv("DB_PORT", "")
+	DB_NAME = GetEnv("DB_NAME", "")
+	DB_USER = GetEnv("DB_USER", "")
+	DB_PASS = GetEnv("DB_PASS", "")
+	DB_SSL = GetEnv("DB_SSL", "enable")
 }
