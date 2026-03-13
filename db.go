f835c63e8657a64cc6eb27a5184dd99d3ec27445
DPC <dyatrumbley@gmail.com>
2026-03-12 22:23:44 -0500

    added global db obj, moved controller query to db func

--- db.go
+++ db.go
@@ -9,6 +9,9 @@ import (
 	_ "github.com/lib/pq"
 )
 
+// global DB obj
+var DB *sql.DB
+
 func Validate_db_config() error {
 	if len(DB_HOST) < 1 ||
 		len(DB_NAME) < 1 ||
@@ -20,14 +23,21 @@ func Validate_db_config() error {
 	return nil
 }
 
-func Open_DB_Connection() (*sql.DB, error) {
+func Open_DB_Connection() error {
+	var err error
 	conn_str := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=%s", DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME, DB_SSL)
-	db, err := sql.Open("postgres", conn_str)
+	DB, err = sql.Open("postgres", conn_str)
 	if err != nil {
 		// logging location for easier debug
 		log.Printf("Error making DB Connection. Err:\n%s\n", err)
-		return nil, err
+		return err
 	}
+	err = DB.Ping()
 	log.Printf("DB Connected Successfully.\n")
-	return db, nil
+	return err
+}
+
+func Query_Book_By_Id(book_id int) *sql.Row {
+	result := DB.QueryRow("SELECT id,title,author FROM BOOK WHERE ID = $1", book_id)
+	return result
 }
