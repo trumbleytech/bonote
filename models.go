2922fc0ce95221fb1f29aaa173223fdeed4e5126
DPC <dyatrumbley@gmail.com>
2026-03-17 21:55:15 -0500

    swapping db nullable properties to pointers to allow null val in scan

--- models.go
+++ models.go
@@ -1,29 +1,31 @@
 package main
 
-import "time"
+import (
+	"time"
+)
 
 type Book struct {
-	Id                uint
-	Title             string
-	Author            string
-	Created_On_UTC    time.Time
-	Last_Modified_UTC time.Time
+	Id              uint
+	Title           string
+	Author          string
+	Created_On_UTC  *time.Time
+	Modified_On_UTC *time.Time
 }
 
 type Chapter struct {
-	Id                uint
-	Book_id           uint
-	Number            uint16
-	Name              string
-	Created_On_UTC    time.Time
-	Last_Modified_UTC time.Time
+	Id              uint
+	Book_id         uint
+	Number          *uint16
+	Name            string
+	Created_On_UTC  *time.Time
+	Modified_On_UTC *time.Time
 }
 
 type Note struct {
-	Id                uint
-	Chapter_id        uint
-	Name              string
-	Content           string
-	Created_On_UTC    time.Time
-	Last_Modified_UTC time.Time
+	Id              uint
+	Chapter_id      uint
+	Name            string
+	Content         string
+	Created_On_UTC  *time.Time
+	Modified_On_UTC *time.Time
 }
