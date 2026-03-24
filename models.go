8cdd08f076d17b5d2276a987be66302b0cfcfa3b
DPC <dyatrumbley@gmail.com>
2026-03-23 23:17:47 -0500

    reverting to base model defs

--- models.go
+++ models.go
@@ -4,13 +4,7 @@ import (
 	"time"
 )
 
-type BookRequest struct {
-	Id     uint   `json:"id"`
-	Title  string `json:"title"`
-	Author string `json:"author,omitempty"`
-}
-
-type BookResponse struct {
+type Book struct {
 	Id              uint       `json:"id"`
 	Title           string     `json:"title"`
 	Author          string     `json:"author,omitempty"`
@@ -18,7 +12,7 @@ type BookResponse struct {
 	Modified_On_UTC *time.Time `json:"modifiedOnUTC,omitempty"`
 }
 
-type ChapterRequest struct {
+type Chapter struct {
 	Id              uint       `json:"id"`
 	Book_id         uint       `json:"bookId"`
 	Number          *uint16    `json:"number,omitempty"`
@@ -27,23 +21,7 @@ type ChapterRequest struct {
 	Modified_On_UTC *time.Time `json:"modifiedOnUTC,omitempty"`
 }
 
-type ChapterResponse struct {
-	Id              uint       `json:"id"`
-	Book_id         uint       `json:"bookId"`
-	Number          *uint16    `json:"number,omitempty"`
-	Name            string     `json:"name,omitempty"`
-	Created_On_UTC  *time.Time `json:"createdOnUTC,omitempty"`
-	Modified_On_UTC *time.Time `json:"modifiedOnUTC,omitempty"`
-}
-
-type NoteRequest struct {
-	Id         uint   `json:"id"`
-	Chapter_id uint   `json:"chapterId"`
-	Name       string `json:"name"`
-	Content    string `json:"content"`
-}
-
-type NoteResponse struct {
+type Note struct {
 	Id              uint       `json:"id"`
 	Chapter_id      uint       `json:"chapterId"`
 	Name            string     `json:"name"`
