7427010b8fdce1e0661b6a76bada42f1104dc3a0
DPC <dyatrumbley@gmail.com>
2026-03-24 21:37:42 -0500

    updating naming conventions

--- models.go
+++ models.go
@@ -5,27 +5,27 @@ import (
 )
 
 type Book struct {
-	Id              uint       `json:"id"`
-	Title           string     `json:"title"`
-	Author          string     `json:"author,omitempty"`
-	Created_On_UTC  *time.Time `json:"createdOnUTC,omitempty"`
-	Modified_On_UTC *time.Time `json:"modifiedOnUTC,omitempty"`
+	Id            uint       `json:"id"`
+	Title         string     `json:"title"`
+	Author        string     `json:"author,omitempty"`
+	CreatedOnUTC  *time.Time `json:"createdOnUTC,omitempty"`
+	ModifiedOnUTC *time.Time `json:"modifiedOnUTC,omitempty"`
 }
 
 type Chapter struct {
-	Id              uint       `json:"id"`
-	Book_id         uint       `json:"bookId"`
-	Number          *uint16    `json:"number,omitempty"`
-	Name            string     `json:"name,omitempty"`
-	Created_On_UTC  *time.Time `json:"createdOnUTC,omitempty"`
-	Modified_On_UTC *time.Time `json:"modifiedOnUTC,omitempty"`
+	Id            uint       `json:"id"`
+	BookID        uint       `json:"bookId"`
+	Number        *uint16    `json:"number,omitempty"`
+	Name          string     `json:"name,omitempty"`
+	CreatedOnUTC  *time.Time `json:"createdOnUTC,omitempty"`
+	ModifiedOnUTC *time.Time `json:"modifiedOnUTC,omitempty"`
 }
 
 type Note struct {
-	Id              uint       `json:"id"`
-	Chapter_id      uint       `json:"chapterId"`
-	Name            string     `json:"name"`
-	Content         string     `json:"content"`
-	Created_On_UTC  *time.Time `json:"createdOnUTC,omitempty"`
-	Modified_On_UTC *time.Time `json:"modifiedOnUTC,omitempty"`
+	Id            uint       `json:"id"`
+	ChapterID     uint       `json:"chapterId"`
+	Name          string     `json:"name"`
+	Content       string     `json:"content"`
+	CreatedOnUTC  *time.Time `json:"createdOnUTC,omitempty"`
+	ModifiedOnUTC *time.Time `json:"modifiedOnUTC,omitempty"`
 }
