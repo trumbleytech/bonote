10847b06da0c06b616db028fe1957882acb0c248
DPC <dyatrumbley@gmail.com>
2026-03-30 19:26:55 -0500

    using gin router groups for clarity

--- main.go
+++ main.go
@@ -20,33 +20,38 @@ func main() {
 	// init gin
 	router := gin.Default()
 
-	// BOOK
-	router.GET("/book/:id", GetBook)
-	router.POST("/book", SaveBook)
-	router.PUT("/book/:id", UpdateBook)
-	router.DELETE("book/:id", DeleteBook)
+	book := router.Group("/book")
+	{
+		book.GET("/:id", GetBook)
+		book.POST("", SaveBook)
+		book.PUT("/:id", UpdateBook)
+		book.DELETE("/:id", DeleteBook)
+		// pull chapters by book id
+		book.GET("/:id/chapters", GetChaptersByBookID)
+	}
 
-	// CHAPTERS BY BOOK
-	router.GET("/book/:id/chapters", GetChaptersByBookID)
-
-	// CHAPTER
-	router.GET("/chapter/:id", GetChapter)
-	router.POST("/chapter", SaveChapter)
-	router.PUT("chapter/:id", UpdateChapter)
-	router.DELETE("chapter/:id", DeleteChapter)
-
-	// NOTES BY CHAPTER
-	router.GET("/chapter/:id/notes", GetNotesByChapterID)
+	chapter := router.Group("/chapter")
+	{
+		chapter.GET("/:id", GetChapter)
+		chapter.POST("", SaveChapter)
+		chapter.PUT("/:id", UpdateChapter)
+		chapter.DELETE("/:id", DeleteChapter)
+		// get notes by chapter id
+		chapter.GET("/:id/notes", GetNotesByChapterID)
+	}
 
 	// NOTE
-	router.GET("/note/:id", GetNote)
-	router.POST("/note", SaveNote)
-	router.PUT("/note/:id", UpdateNote)
-	router.DELETE("/note/:id", DeleteNote)
+	note := router.Group("/note")
+	{
+		note.GET("/:id", GetNote)
+		note.POST("", SaveNote)
+		note.PUT("/:id", UpdateNote)
+		note.DELETE("/:id", DeleteNote)
+	}
 
 	// start the server
 	fmt.Printf("Starting server on port %s...\n", PORT)
 	if err := router.Run(":" + PORT); err != nil {
-		fmt.Printf("Server failed to start. Error:\n%s", err)
+		log.Printf("Server failed to start. Error:\n%s", err)
 	}
 }
