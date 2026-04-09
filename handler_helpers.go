a43e71dfa6ef2a9a822e47d3275c5de01bc7e942
DPC <dyatrumbley@gmail.com>
2026-04-08 22:46:15 -0500

    QoL helper funcs

--- handler_helpers.go
+++ handler_helpers.go
@@ -6,8 +6,23 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+func HandleForbidden(context *gin.Context) {
+	context.JSON(http.StatusForbidden, gin.H{
+		"message": "Action forbidden.",
+	})
+}
 func HandleInternalServerError(context *gin.Context) {
 	context.JSON(http.StatusInternalServerError, gin.H{
 		"message": "Internal Server Error. Please try again later.",
+	})
+}
+
+func UserCanModifyResource(usermin UserMin, resourceUserID uint) bool {
+	return usermin.Role == "admin" || usermin.Id == int(resourceUserID)
+}
+
+func HandleNotLoggedIn(context *gin.Context) {
+	context.JSON(http.StatusUnauthorized, gin.H{
+		"message": "You are not logged in.",
 	})
 }
